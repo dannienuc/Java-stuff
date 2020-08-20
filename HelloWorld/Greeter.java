60a61b5d44029cb0205035e4154a89f9147efcec
Dan Campbell <dannienuc@yahoo.com>
2020-08-20 16:01:57 -0400

    update

---
+++ HelloWorld/Greeter.java
@@ -0,0 +1,9 @@
+//class to create the Greeter object type
+
+public class Greeter
+{
+    public void sayHello()
+    {
+        System.out.println("Hello World !!!!");
+    }
+}
---
+++ HelloWorld/HelloWorldOOP.java
@@ -0,0 +1,11 @@
+//Application to demonstrate the OOP methodology for implementing a Hello World 
+//style class
+
+public class HelloWorldOOP
+{
+
+}public static void main(String[] args)
+{
+    Greeter myGreeterObject = new Greeter();
+    myGreeterObjHello();
+}
